e090196086942265d94675090f09bcbc87fca6c1
diego-nino <99043882+diego-nino@users.noreply.github.com>
2022-10-27 19:50:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
 
-@app.route('/checkIn-hardware/<projectId>/<qty>', methods=["GET"])
+@app.route('/checkIn-hardware', methods=["GET"])
 def checkIn_hardware(projectID, qty):
     data = {
         "value": qty + " hardware checked in"
@@ -21,7 +21,7 @@ def checkIn_hardware(projectID, qty):
     return data
 
 
-@app.route('/checkOut-hardware/<projectid>/<qty>', methods=["GET"])
+@app.route('/checkOut-hardware', methods=["GET"])
 def checkOut_hardware(projectid, qty):
     data = {
         "value": qty + " hardware checked out"
@@ -29,7 +29,7 @@ def checkOut_hardware(projectid, qty):
     return data
 
 
-@app.route('/joinProject/<projectid>', methods=["GET"])
+@app.route('/joinProject', methods=["GET"])
 def joinProject(projectid):
     data = {
         "value": "Joined " + projectid
@@ -37,7 +37,7 @@ def joinProject(projectid):
     return data
 
 
-@app.route('/leaveProject/<projectid>', methods=["GET"])
+@app.route('/leaveProject', methods=["GET"])
 def leaveProject(projectid):
     data = {
         "value": "Left " + projectid
